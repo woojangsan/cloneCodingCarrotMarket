17ef7363debde3c0c64f95078fc49fa071d9155d
ctr0812 <andy0812@naver.com>
2022-08-23 09:35:23 +0900

    수정 사항 반영

--- src/main/java/springc1/clonecoding/controller/request/SignupRequestDto.java
+++ src/main/java/springc1/clonecoding/controller/request/SignupRequestDto.java
@@ -4,13 +4,30 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class SignupRequestDto {
 
+
+    @NotBlank
+    @Size(min = 4, max = 12)
+    @Pattern(regexp = "[a-zA-Z\\d]*${3,12}")
     private String username;
+
+    @NotBlank
+    @Size(min = 4, max = 12)
+    @Pattern(regexp = "[a-zA-Z\\d]*${3,12}")
     private String nickname;
+
+    @NotBlank
+    @Size(min = 4, max = 32)
+    @Pattern(regexp = "[a-z\\d]*${3,32}")
     private String password;
+
     private String location;
 }
